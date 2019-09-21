0d46a203097c22fcbcf5c0333fb2b298c6b76cd5
Peter Bryzgalov <peterbryz@yahoo.com>
2019-09-21 23:41:20 +0900

    Benchmark VGG16_keras model on QuadroP2000

--- benchmark/run_series.py
+++ benchmark/run_series.py
@@ -4,7 +4,7 @@ import time
 import os
 
 gpus = range(0, 1)
-batchsizes = [7, 8, 9] + range(10, 251, 10)
+batchsizes = [7, 8, 9] + range(10, 200, 10) + range(200, 501, 50)
 # CHANGE HOST NAME
 host = "mouse"
 
@@ -13,7 +13,7 @@ gpu_model = multigpuexec.getGPUinfo(0, query="name", get_cuda=False).strip().rep
 print gpu_model
 
 # imgsize = 224
-command = "python benchmark.py --testVGG16 --no_timeline --saveto results --iter_benchmark 50 --device {} --comment {}".format(
+command = "python benchmark.py --testVGG16 --no_timeline --saveto results --iter_benchmark 10 --imgsize 32 --keras --device {} --comment {}".format(
     gpu_model, host)
 tasks = []
 
